5bebb1e1555ee007b26af2a9e90b2a98adefe329
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-07-13 21:11:46 +0200

    Update exo_4.py

--- exo_4.py
+++ exo_4.py
@@ -1,1 +1,2 @@
-
+villes = ['Bordeaux','Brest','Limoges','Lyon','Marseille','Nantes','Nice','Paris','Pau','La Rochelle','Strasbourg','Tours']
+ensoll = [2031,1676,1932,2145,2773,2101,2504,1995,1821,2424,2124,2050]
